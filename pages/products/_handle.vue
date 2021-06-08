e1d4937441169ef75cad0bb1cdeaa4a18bf1408e
Scott Mallin <scott@scottmallin.com>
2021-06-08 22:25:55 +0100

    upd: options and variants

--- pages/products/_handle.vue
+++ pages/products/_handle.vue
@@ -10,6 +10,25 @@
     </div>
     <div class="w-full md:w-1/2">
       <h1>{{ product.title }}</h1>
+      <form v-if="product.options" action="" method="post">
+        <div v-for="option in product.options" :key="option.id">
+          <label :for="'options_' + option.name">{{ option.name }}</label>
+          <select
+            @change="optionSelect($event, option.name)"
+            name="options"
+            :id="'options_' + option.name"
+          >
+            <option value="">--Select--</option>
+            <option
+              v-for="value in option.values"
+              :key="value.index"
+              :value="value.value"
+            >
+              {{ value.value }}
+            </option>
+          </select>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -19,12 +38,25 @@ export default {
   data() {
     return {
       handle: this.$route.params.handle,
+      selectedOptions: {},
+      selectedVariant: '',
     }
+  },
+  methods: {
+    optionSelect(e, optionName) {
+      this.selectedOptions[optionName] = e.target.value
+      this.calculateVariant(this.selectedOptions)
+    },
+    calculateVariant(options) {
+      console.log('calculateVariant', options)
+      options.filter((option) => {})
+    },
   },
   async asyncData({ $shopify, params }) {
     const product = await $shopify.product.fetchByHandle(params.handle)
-    console.log(product.variants)
-    return { product }
+    const options = product.options
+    const variants = product.variants
+    return { product, options, variants }
   },
 }
 </script>
